035b86b56d9ee2a5f839d0227ef8f2b9ec2a9d85
EvaAngel <1358823605@qq.com>
2017-01-24 19:46:55 +0800

    Add files via upload

---
+++ src/com/fuxin/javabean/Student.java
@@ -0,0 +1,47 @@
+package com.fuxin.javabean;
+
+public class Student {
+private String username;
+private String password;
+private String name;
+private String age;
+private String classes;
+private String sex;
+public String getSex() {
+	return sex;
+}
+public void setSex(String sex) {
+	this.sex = sex;
+}
+public String getClasses() {
+	return classes;
+}
+public void setClasses(String classes) {
+	this.classes = classes;
+}
+public String getAge() {
+	return age;
+}
+public void setAge(String age) {
+	this.age = age;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getUsername() {
+	return username;
+}
+public void setUsername(String username) {
+	this.username = username;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+}
